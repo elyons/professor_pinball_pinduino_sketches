bcc75791b729790a82853d73f7d253a04e42ad24
elyons <lyonsden@gmail.com>
2015-02-05 12:39:30 -0700

    initial import

---
+++ pinduino/src/pinduinoPins.h
@@ -0,0 +1,33 @@
+/*
+  pinduino.h - Library for pinduino shield for Arduinos.
+  Created by Eric Lyons and Wes Sparks, 2015.
+  Released into the public domain.
+  Play more pinball!
+*/
+#ifndef pinduinoPins_h
+#define pinduinoPins_h
+
+#include <Arduino.h>
+class pinduinoPins
+{
+  protected:
+
+  public:
+    //Constructor
+    pinduinoPins();
+		
+		//Arduino pin functions
+		void resetPinStates();
+		void printPinStates();
+		void updatePinStates();
+
+  private:
+		//internal array for input pins getting signal from pinball machine
+		const static int _numPins = 16;
+		int _pins[_numPins] = { 22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52 };
+		int _pinStates[_numPins];
+};
+
+
+
+#endif
