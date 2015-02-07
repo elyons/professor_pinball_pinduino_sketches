ac2080c0a6d682e7fe441b97c750a222927ffd69
elyons <lyonsden@gmail.com>
2015-02-07 07:24:29 -0700

    added comments and new functions resetPin and any

--- pinduino/src/pinduinoPins.h
+++ pinduino/src/pinduinoPins.h
@@ -17,13 +17,15 @@ class pinduinoPins
     pinduinoPins();
 		
 		//Arduino pin functions
-		void reset();
-		void print();
-		void update();
-		int get(int pin);
-		int numPins();
-		int J6(int pin);
-		int J7(int pin);
+		void reset(); // resets values of all pin states to 0
+		void resetPin(int pin); // resets the value of a pin State to 0
+		void print(); // print values of pin states
+		void update(); // update pin states by scanning input pins
+		int get(int pin); // gets the pin state for a given Arduino pin
+		int numPins(); // returns the number of pins being monitors (default: 16)
+		int J6(int pin); // interface to retrieve pin state by J6 pin
+		int J7(int pin); // interface to retrieve pin state by J7 pin 
+		int any(); // returns true if any pinStates have non-zero values
 
   private:
 		//internal array for input pins getting signal from pinball machine
