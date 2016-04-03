05bc8f8b454c7b09f752b51c68facbb29ea95750
elyons <lyonsden@gmail.com>
2016-04-03 15:35:36 -0700

    updated tp use Nano based pinduino

--- BOP-ramp-head/BOP-ramp-head.ino
+++ BOP-ramp-head/BOP-ramp-head.ino
@@ -12,7 +12,7 @@ int aLEDNum1 = 46; //ramp
 int aLEDNum2 = 7; //head
 int aLEDNum3 = 1;
 
-pinduino pd (aLEDNum1, aLEDNum2, aLEDNum3);
+pinduino pd (aLEDNum1, aLEDNum2, aLEDNum3, "Nano");
 
 
 
@@ -51,7 +51,7 @@ void checkPinStates(){
   
 
   
-  if ( pd.pinState()->J6(1) ){ // billion
+  if ( pd.pinState()->J126(1) ){ // billion
     for (int i=0; i<100; i++) {
      pd.adrLED2()->chase2Color("red", "blue", 1,  20,  -1);
      pd.adrLED2()->chase2Color("blue", "red", 1,  20,  1);
@@ -59,7 +59,7 @@ void checkPinStates(){
     trigger = 1; 
   }
 
- if ( pd.pinState()->J6(2) ){ //left ramp shuttle
+ if ( pd.pinState()->J126(2) ){ //left ramp shuttle
 //    pd.adrLED1()->color("blue", 255);
     pd.adrLED2()->color("blue", 255);
     pd.adrLED1()->chase("blue", 2,  5,  -1);
@@ -67,16 +67,16 @@ void checkPinStates(){
     trigger = 1; 
   }
 
-  if ( pd.pinState()->J6(3) ){ //jackpot
+  if ( pd.pinState()->J126(3) ){ //jackpot
     pd.adrLED1()->chase2ColorFromPoint(23, "green", "red", 10,5);
     trigger = 1; 
   }
-  if ( pd.pinState()->J6(4) ){ // right shuttle skill shot
+  if ( pd.pinState()->J126(4) ){ // right shuttle skill shot
     pd.adrLED2()->color("red", 255);
     pd.adrLED2()->fadeOut(200);
     trigger=1;
   }
-  if ( pd.pinState()->J6(5) ){ // left helmet
+  if ( pd.pinState()->J126(5) ){ // left helmet
     pd.adrLED2()->color("yellow", 255);
     delay(100);
     pd.adrLED2()->clear();    
@@ -84,20 +84,20 @@ void checkPinStates(){
     trigger = 1; 
   }
   
-  if ( pd.pinState()->J6(6) ){ // right helmet
+  if ( pd.pinState()->J126(6) ){ // right helmet
     pd.adrLED2()->color("purple", 255);
     delay(100);
     pd.adrLED2()->clear();    
     trigger = 1; 
   }
   
-  if ( pd.pinState()->J6(7) ){ //right boob
+  if ( pd.pinState()->J126(7) ){ //right boob
     pd.adrLED2()->color("red", 255);
     delay(100);
     pd.adrLED2()->clear();
     trigger = 1; 
   }
-  if ( pd.pinState()->J6(8) && ! pd.pinState()->J6(7) ){ // 8==left boob, 7==right boob
+  if ( pd.pinState()->J126(8) && ! pd.pinState()->J126(7) ){ // 8==left boob, 7==right boob
     pd.adrLED2()->color("blue", 255);
     pd.adrLED1()->chase2Color("red", "orange", 60,  1,  -1);
     pd.adrLED2()->clear();
