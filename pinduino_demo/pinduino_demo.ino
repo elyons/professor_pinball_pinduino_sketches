870928119f6be37fbd94ecd232909c0db67934a1
elyons <lyonsden@gmail.com>
2017-07-14 11:57:40 -0700

    update for arduino nano, made very simple test

--- pinduino_demo/pinduino_demo.ino
+++ pinduino_demo/pinduino_demo.ino
@@ -1,25 +1,36 @@
 #include <pinduino.h>
 
 
-int aLEDNum1 = 300;
-int aLEDNum2 = 300;
-int aLEDNum3 = 300;
+int aLEDNum1 = 100;
+int aLEDNum2 = 10;
+int aLEDNum3 = 10;
 
-pinduino pd(aLEDNum1, aLEDNum2, aLEDNum3);
+//pinduino pd(aLEDNum1, aLEDNum2, aLEDNum3);
+
+pinduino pd(aLEDNum1, aLEDNum2, aLEDNum3, "Nano");
+
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println("Starting");
   pd.adrLED1()->clear();
   pd.adrLED2()->clear();
-  pd.adrLED3()->clear();
   pd.pinState()->reset();
-//  Serial.println("1");
 //  delay(1000);
 }
 
 
+int i =0;
 void loop()
 {
+  Serial.println(i);
+  i=i+1;
 //  pd.pinState()->update();
-  pd.testAdrLED(pd.adrLED1());
+//  pd.testAdrLED(pd.adrLED1());
+   pd.adrLED1()->color("green",255);
+   pd.adrLED2()->color("green",255);
+   delay(1000);
+   pd.adrLED1()->clear();
+   pd.adrLED2()->clear();
+   delay(1000);
 }
