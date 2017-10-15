562c8946321389878d75f4192a1bea031e01c711
elyons <lyonsden@gmail.com>
2017-10-14 19:53:58 -0700

    added support for ramp LEDS on LED channel 2

--- Sega-Baywatch/Sega-Baywatch.ino
+++ Sega-Baywatch/Sega-Baywatch.ino
@@ -6,11 +6,11 @@
 #include <pinduino.h>
 
 int aLEDNum1 = 28;
-int aLEDNum2 = 50;
+int aLEDNum2 = 35;
 
 pinduino pd (aLEDNum1, aLEDNum2, "Nano");
 
-int bg_chase_on = 0;
+int bg_chase_on = 1;
 unsigned long timeLastEvent = 0; // time last event was last triggered
 int startChaseWaitTime = 20000; //Amount of time to wait before chase lights start up again 1000 == 1 second
 int whiteWaitTime = 200; //Amount of time to wait before gumball goes to white
@@ -20,6 +20,7 @@ void setup() {
   pd.adrLED1()->clear();
   pd.adrLED2()->clear();
   pd.pinState()->reset();
+  pd.adrLED1()->color("white", 255);
 }
 
 void loop(){
@@ -39,26 +40,21 @@ void checkPinStates(){
   
   if ( pd.pinState()->J126(1) ){
     pd.adrLED1()->color("yellow",255); // boat right flasher yellow
-    delay(300);
+    pd.adrLED2()->bullet2Color("blue", "white", 30,7,-1);
     trigger=1;
   }
   if ( pd.pinState()->J126(2) ){ 
-    pd.adrLED1()->color("red",255);
-    delay(500);
     trigger=1;
   }
   if ( pd.pinState()->J126(3) ){ 
-    pd.adrLED1()->color("green",255);
-    delay(500);
     trigger=1;
   }
   if ( pd.pinState()->J126(4) ){ 
-    pd.adrLED1()->color("blue",255);
-    delay(500);
     trigger=1;
   }
   if ( pd.pinState()->J126(5) ){ //Venice Beach ramp and right two ramp domes
     pd.adrLED1()->color("sky",255);
+    pd.adrLED2()->bullet2Color("white", "blue", 30,7,1);
     delay(300);
     trigger=1;
   }
@@ -67,38 +63,35 @@ void checkPinStates(){
     delay(300);
     trigger=1;
   }
-  if ( pd.pinState()->J126(7) ){ //right tital wave (not registering)
+  if ( pd.pinState()->J126(7) ){ //right tital wave 
     pd.adrLED1()->color("blue",255);
     delay(500);
     trigger=1;
   }
   if ( pd.pinState()->J126(8) ){ 
     pd.adrLED1()->color("green",255); // pop inserts
+//    pd.adrLED2()->color("green",255); // pop inserts
     delay(200);
     trigger=1;
   }
   if ( pd.pinState()->J126(9) ){ 
-    pd.adrLED1()->color("green",255);
-    delay(500);
     trigger=1;
   }
   if ( pd.pinState()->J126(10) ){ 
-    pd.adrLED1()->color("blue",255);
-    delay(500);
     trigger=1;
   }
   if ( pd.pinState()->J126(11) ){ 
-    pd.adrLED1()->color("red",255);
-    delay(500);
     trigger=1;
   }
   if ( pd.pinState()->J126(12) ){ 
-    pd.adrLED1()->color("blue",255);
-    delay(500);
     trigger=1;
   }
   if ( pd.pinState()->J126(13) ){ // Flash: dome flashers on back panel
     pd.adrLED1()->color("red",255);
+    pd.adrLED2()->spreadInFromPoint2RGB (14, 150,150,150, 0,0,255, 300);
+    delay(500);
+    pd.adrLED2()->fadeOut(500);    
+
     delay(200);
     trigger=1;
   }
@@ -113,9 +106,7 @@ void checkPinStates(){
     delay(300);
     trigger=1;
   }
-  if ( pd.pinState()->J126(16) ){  //extended WPC with extra 4-pin plug
-    pd.adrLED1()->color("green",255);
-    delay(300);
+  if ( pd.pinState()->J126(16) ){  
     trigger=1;
   }
 
@@ -135,31 +126,27 @@ void checkPinStates(){
 
 
 void backgroundChase() {
-  pd.adrLED1()->clear();
-  pd.adrLED1()->color("white",255);
-  pd.adrLED2()->color("white",255);
   for (int i = 0; i < 20000; i = i + 1) { //check pinstates for a while
     pd.pinState()->update();
   }
-  pd.adrLED1()->fadeOut(1000);
   pd.pinState()->update();
   delay(500);
   pd.pinState()->update(); 
-  pd.adrLED1()->fadeIn("blue",500);
+  pd.adrLED1()->fadeColor2Color("white","blue",500);
   pd.pinState()->update();
-  pd.adrLED1()->fadeColor2Color("blue","purple",500);
+  pd.adrLED1()->fadeColor2Color("blue","red",500);
+  pd.adrLED2()->bullet2Color("blue", "red", 30,30,1);
   pd.pinState()->update();
-  pd.adrLED1()->fadeColor2Color("purple","red",500);
-  pd.pinState()->update();
-  pd.adrLED1()->fadeColor2Color("red","orange",500);
-  pd.pinState()->update();
-  pd.adrLED1()->fadeColor2Color("orange","yellow",500);
+  pd.adrLED1()->fadeColor2Color("red","yellow",500);
+  pd.adrLED2()->bullet2Color("red", "yellow", 30,30,-1);
   pd.pinState()->update();
   pd.adrLED1()->fadeColor2Color("yellow","green",500);
+  pd.adrLED2()->bullet2Color("yellow", "green", 30,30,1);
   pd.pinState()->update();
   pd.adrLED1()->fadeColor2Color("green","blue",500);
+  pd.adrLED2()->bullet2Color("green", "blue", 30,30,-1);
   pd.pinState()->update();
-  pd.adrLED1()->fadeOut(500);
+  pd.adrLED1()->fadeColor2Color("blue", "white", 500);
   pd.pinState()->update();
   delay(500);
   pd.pinState()->update();
