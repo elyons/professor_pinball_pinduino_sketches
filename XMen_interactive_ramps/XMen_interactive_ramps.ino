ed8008f43c9393ab42cfa9cf2d8982dc86affeb3
elyons <lyonsden@gmail.com>
2017-10-29 10:31:35 -0700

    extensive update

--- XMen_interactive_ramps/XMen_interactive_ramps.ino
+++ XMen_interactive_ramps/XMen_interactive_ramps.ino
@@ -1,17 +1,17 @@
 //Eric Lyons 2015
 //Note to self:  Play more pinball!
-//Interfaced for pinduino shield v0.2
+//Interfaced for pinduino shield v0.3
 //Uses pinduino library
 
 #include <pinduino.h>
-int N_LEDS = 151;
-int R_START = 82;
+int N_LEDS = 107;
+int R_START = 67;
 
 int aLEDNum1 = N_LEDS;
 int aLEDNum2 = 0;
 int aLEDNum3 = 0;
 
-pinduino pd (aLEDNum1, aLEDNum2, aLEDNum3);
+pinduino pd (aLEDNum1, aLEDNum2, aLEDNum3, "Nano");
 
 // pin 0:  Flash: Pop Bumper Fl. (BLK-BRN)
 // pin 1:  Orbit Diverter (BLK-RED)
@@ -32,7 +32,10 @@ pinduino pd (aLEDNum1, aLEDNum2, aLEDNum3);
 
 int bg_chase_on = 1;
 unsigned long timeLastEvent = 0; // time last event was last triggered
-int startChaseWaitTime = 15000; //Amount of time to wait before chase lights start up again 1000 == 1 second
+int startChaseWaitTime = 60000; //Amount of time to wait before chase lights start up again 1000 == 1 second
+int bgWaitTime = 300; //Amount of time to wait before backglass turns on
+int bgOn=1;
+String color = "white"; //attract color
 
 void setup() {
   Serial.begin(115200);
@@ -48,6 +51,9 @@ void loop(){
 //   Print the pin states out to serial 
 //  pd.pinState()->print();
   checkPinStates();
+  if (millis()-timeLastEvent > bgWaitTime && bgOn == 0) {
+    bgOn =1;
+  }
   if (millis()-timeLastEvent > startChaseWaitTime) {bg_chase_on=1;}
 }
 
@@ -55,57 +61,87 @@ void checkPinStates(){
   int trigger =0;
   
   if ( pd.pinState()->J6(1) ){
+    pd.adrLED1()->fadeOut(10);
+    pd.adrLED1()->bullet2Color("red", "yellow", 3, 0, -1);
+    pd.adrLED1()->bullet2Color("yellow", "red", 3, 0, 1);
+//    pd.adrLED1()->chase2RGB(0,0,255, 255,255,255,N_LEDS/6, 0, 1);
+    trigger =1;
   }
   if ( pd.pinState()->J6(2) ){ 
-  }
-  if ( pd.pinState()->J6(3) ){ 
-    pd.adrLED1()->chase2RGB(0,0,255, 255,255,255,N_LEDS/6, 0, -1);
+    pd.adrLED1()->fadeOut(10);
+    pd.adrLED1()->color("blue");
     trigger =1;
   }
-  if ( pd.pinState()->J6(4) ){ 
-    pd.adrLED1()->spreadInFromPointRGB(R_START,100, 100, 255, 0);
-    pd.adrLED1()->fadeOut(100);
+  if ( pd.pinState()->J6(3) ){
+    pd.adrLED1()->fadeOut(10); 
+    pd.adrLED1()->chase2RGB(R_START, 0,0,255, 255,255,255, 0, -1);
     trigger =1;
+  }
+  if ( pd.pinState()->J6(4) ){
+//    pd.adrLED1()->fadeOut(10); 
+//    pd.adrLED1()->spreadInFromPointRGB(R_START,100, 100, 255, 0);
+//    pd.adrLED1()->fadeOut(100);
+//    trigger =1;
   }
   if ( pd.pinState()->J6(5) ){
-    pd.adrLED1()->spreadInFromPointRGB(R_START,255, 100, 0, 0);
-    pd.adrLED1()->fadeOut(100);
-    trigger =1;
+//    pd.adrLED1()->fadeOut(10);
+//    pd.adrLED1()->spreadInFromPointRGB(R_START,255, 100, 0, 0);
+//    pd.adrLED1()->fadeOut(100);
+//    trigger =1;
   }
   if ( pd.pinState()->J6(6)  and bg_chase_on==0 ){
-    pd.adrLED1()->color("red",255);
+    pd.adrLED1()->fadeOut(10);
+//    pd.adrLED1()->color("red",255);
+    pd.adrLED1()->spreadInFromPoint (R_START, "red", 0);
     pd.adrLED1()->spreadOutFromPoint (R_START, 0);
     trigger =1;
   }
   if ( pd.pinState()->J6(7) ){
+    pd.adrLED1()->fadeOut(10);
     pd.adrLED1()->color("yellow", 255);
-    delay(20);
+    delay(100);
     pd.adrLED1()->clear();
     trigger =1;
   }
-  if ( pd.pinState()->J6(8) ){ 
+  if ( pd.pinState()->J6(8) ){
+    pd.adrLED1()->fadeOut(10); 
     pd.adrLED1()->color("red", 255);
-    delay(20);
+    delay(100);
     trigger =1;
   }
-  if ( pd.pinState()->J7(6) ){ 
-      trigger =1;
-  }
   if ( pd.pinState()->J7(2) ){
-      pd.adrLED1()->chase2Color("blue", "yellow", N_LEDS/8, 0, -1);
-      trigger=1;
+    pd.adrLED1()->fadeOut(10);
+    pd.adrLED1()->bullet2Color("blue", "yellow", N_LEDS/10, 0, -1);
+    trigger=1;
   }
-  if ( pd.pinState()->J7(3) ){ 
-      pd.adrLED1()->chase2Color("red", "blue",N_LEDS/8, 0, 1);
-      trigger =1;
+  if ( pd.pinState()->J7(3) ){
+    pd.adrLED1()->fadeOut(10); 
+    pd.adrLED1()->bullet2Color("yellow", "blue",N_LEDS/10, 0, 1);
+    trigger =1;
   }
   if ( pd.pinState()->J7(4) ){
-      pd.adrLED1()->chase2Color("yellow", "blue",N_LEDS/4, 0, 1);
-      trigger =1;
+    pd.adrLED1()->fadeOut(10);
+    pd.adrLED1()->spreadInFromPoint2RGB (R_START, 255,0,0, 50,0,0, 0);
+    delay(100);
+    pd.adrLED1()->fadeOut(50);
+    trigger =1;
+  }
+  if ( pd.pinState()->J7(6) ){
+    pd.adrLED1()->fadeOut(10);
+    pd.adrLED1()->spreadInFromPoint2RGB (R_START, 0,0,255, 0,0,50, 0);
+    delay(100);
+    pd.adrLED1()->fadeOut(50);
+    trigger =1;
   }
   if ( pd.pinState()->J7(7) ){
+    pd.adrLED1()->fadeOut(10);
+    pd.adrLED1()->color("yellow");
+    delay(100);
+    pd.adrLED1()->fadeOut(50);
+    trigger =1;
   }
   if ( pd.pinState()->J7(8) ){
+    pd.adrLED1()->fadeOut(10);
     pd.adrLED1()->spreadInFromPoint2Color (R_START, "red", "blue", 0);
     pd.adrLED1()->fadeOut(100);
     trigger=1;
@@ -117,11 +153,12 @@ void checkPinStates(){
 
 //trigger is to take care of any cleanup after a sequence has been triggered.
   if (trigger) {
+  if (pd.pinState()->J6(6) == 0) timeLastEvent = millis();
    pd.adrLED1()->clear();
    pd.pinState()->reset();
    trigger =0;
    bg_chase_on = 0;
-   timeLastEvent = millis();
+   bgOn=0;
   }
 
 //end function checkPinStates
@@ -130,21 +167,28 @@ void checkPinStates(){
 
 
 void backgroundChase() {
-  pd.adrLED1()->spreadInFromPoint2Color(R_START,"sky", "blue", 10);
-  pd.adrLED1()->fadeOut(100);
-  delay(10);
-  pd.adrLED1()->color("blue",150);
-  pd.adrLED1()->fadeOut(50);
-  delay(100);
-  pd.adrLED1()->color("blue",100);
-  pd.adrLED1()->fadeOut(50);
-  delay(200);
-  pd.adrLED1()->color("blue",50);
-  pd.adrLED1()->fadeOut(200);
-  for (int i=0; i<100; i=i+1){
-      pd.pinState()->update();
-      delay(10);
+  pd.adrLED1()->sparkle(color,20);
+  if (random(1000) == 0) {
+      if (color == "blue") color = "red";
+      else if (color == "red") color = "white";
+      else if (color == "white") color = "yellow";
+      else color = "blue";
   }  
+//  pd.adrLED1()->spreadInFromPoint2Color(R_START,"yellow", "blue", 10);
+//  pd.adrLED1()->fadeOut(100);
+//  delay(10);
+//  pd.adrLED1()->color("blue");
+//  pd.adrLED1()->fadeOut(50);
+//  delay(100);
+//  pd.adrLED1()->colorRGB(0,0,100);
+//  pd.adrLED1()->fadeOut(50);
+//  delay(200);
+//  pd.adrLED1()->colorRGB(0,0,50);
+//  pd.adrLED1()->fadeOut(200);
+//  for (int i=0; i<100; i=i+1){
+//      pd.pinState()->update();
+//      delay(10);
+//  }  
 }
 
 
