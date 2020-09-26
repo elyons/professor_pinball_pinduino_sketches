cf91b2a7cfa28707a56987dbfdae7184baee073b
Eric Lyons <elyons@Erics-iMac.local>
2020-09-26 12:10:06 -0700

    updated code formating

--- Star-Trek-TNG-Borg-Ship-plus-cabinet/Star-Trek-TNG-Borg-Ship-plus-cabinet.ino
+++ Star-Trek-TNG-Borg-Ship-plus-cabinet/Star-Trek-TNG-Borg-Ship-plus-cabinet.ino
@@ -26,77 +26,77 @@ void setup() {
   pd.pinState()->reset();
 }
 
-void loop(){
-//  if (attractOn){attractMode();}
+void loop() {
+  //  if (attractOn){attractMode();}
   attractMode();
   pd.pinState()->update();
-//   Print the pin states out to serial 
-//  pd.pinState()->print();
+  //   Print the pin states out to serial
+  //  pd.pinState()->print();
   checkPinStates();
- // if (millis()-timeLastEvent > attractModeTime) {attractOn=1;}
+  // if (millis()-timeLastEvent > attractModeTime) {attractOn=1;}
 }
 
-void checkPinStates(){
-  int trigger =0;
+void checkPinStates() {
+  int trigger = 0;
 
-  if ( pd.pinState()->J126(1) ){  //  Left Gun Motor
+  if ( pd.pinState()->J126(1) ) { //  Left Gun Motor
     pd.adrLED1()->color("red");
-    pd.adrLED1()->fadeIn("red",50);
+    pd.adrLED1()->fadeIn("red", 50);
     delay(2000);
     pd.adrLED2()->clear();
     pd.adrLED1()->fadeOut(50);
-    trigger =1;
+    trigger = 1;
   }
-  if ( pd.pinState()->J126(2) ){ //  Right Gun Motor
+  if ( pd.pinState()->J126(2) ) { //  Right Gun Motor
     pd.adrLED1()->color("red");
-    pd.adrLED1()->fadeIn("red",50);
+    pd.adrLED1()->fadeIn("red", 50);
     delay(2000);
     pd.adrLED2()->clear();
     pd.adrLED1()->fadeOut(50);
-    trigger =1;
+    trigger = 1;
   }
-  if ( pd.pinState()->J126(4) ){ //Jets
+  if ( pd.pinState()->J126(4) ) { //Jets
     pd.adrLED1()->color("purple");
     pd.adrLED2()->color("purple");
     delay(200);
     pd.adrLED1()->fadeOut(100);
     pd.adrLED2()->clear();
-    trigger =1;
+    trigger = 1;
   }
-  if ( pd.pinState()->J126(5) ){ //Right Popper 
+  if ( pd.pinState()->J126(5) ) { //Right Popper
     pd.adrLED1()->color("green");
     pd.adrLED2()->color("green");
     delay(200);
     pd.adrLED1()->fadeOut(100);
     pd.adrLED2()->fadeOut(100);
-    trigger =1;
+    trigger = 1;
   }
-  if ( pd.pinState()->J126(6) ){ //Middle Ramp 
+  if ( pd.pinState()->J126(6) ) { //Middle Ramp
     pd.adrLED1()->color("cyan");
     pd.adrLED2()->color("cyan");
     delay(200);
     pd.adrLED1()->fadeOut(100);
     pd.adrLED2()->fadeOut(100);
-    trigger =1;
+    trigger = 1;
   }
-  if ( pd.pinState()->J126(7) ){ //Shields
+  if ( pd.pinState()->J126(7) ) { //Shields
     pd.adrLED1()->color("red");
     pd.adrLED2()->color("blue");
     delay(200);
     pd.adrLED1()->fadeOut(100);
     pd.adrLED2()->fadeOut(100);
-    trigger =1;
+    trigger = 1;
   }
-  if ( pd.pinState()->J126(8) ){ //Autofire
-//    trigger=1;
+  if ( pd.pinState()->J126(8) ) { //Autofire
+    //    trigger=1;
   }
-  if ( pd.pinState()->J126(13) ){ // Exit Underground
+  if ( pd.pinState()->J126(13) ) { // Exit Underground
     pd.adrLED1()->color("blue");
     pd.adrLED2()->color("blue");
     delay(200);
     pd.adrLED1()->fadeOut(100);
     pd.adrLED2()->fadeOut(100);
-    trigger =1;
+    trigger = 1;
   }
   if ( pd.pinState()->J126(14) and pd.pinState()->J126(15)) { //both left/right borg flashers are going
     pd.adrLED1()->color("red");
@@ -104,50 +104,50 @@ void checkPinStates(){
     delay(200);
     pd.adrLED1()->fadeOut(100);
     pd.adrLED2()->fadeOut(100);
-    trigger =1;
+    trigger = 1;
   }
-  else if ( pd.pinState()->J126(14) ){ //Right Borg ship
+  else if ( pd.pinState()->J126(14) ) { //Right Borg ship
     pd.adrLED1()->color("red");
     pd.adrLED2()->color("red");
     delay(200);
-    pd.adrLED1()->fadeOut(100);   
+    pd.adrLED1()->fadeOut(100);
     pd.adrLED2()->fadeOut(100);
-    trigger =1;
+    trigger = 1;
   }
-  else if ( pd.pinState()->J126(15) ){ //Left Borg ship
+  else if ( pd.pinState()->J126(15) ) { //Left Borg ship
     pd.adrLED1()->color("orange");
     pd.adrLED2()->color("orange");
     delay(200);
     pd.adrLED1()->fadeOut(100);
     pd.adrLED2()->fadeOut(100);
-    trigger =1;
+    trigger = 1;
   }
-  if ( pd.pinState()->J126(16)){ // Center Borg ship
+  if ( pd.pinState()->J126(16)) { // Center Borg ship
     pd.adrLED1()->color("yellow");
     pd.adrLED2()->color("yellow");
     delay(200);
-    pd.adrLED1()->fadeOut(100);  
+    pd.adrLED1()->fadeOut(100);
     pd.adrLED2()->fadeOut(100);
-    trigger =1;
-  }
- 
-//trigger is to take care of any cleanup after a sequence has been triggered.
-  if (trigger) {
-   pd.adrLED1()->clear();
-   pd.adrLED2()->clear();
-   pd.pinState()->reset();
-   trigger =0;
-   attractOn = 0;
-   timeLastEvent = millis();
-   color="green";
+    trigger = 1;
   }
 
-//end function checkPinStates
+  //trigger is to take care of any cleanup after a sequence has been triggered.
+  if (trigger) {
+    pd.adrLED1()->clear();
+    pd.adrLED2()->clear();
+    pd.pinState()->reset();
+    trigger = 0;
+    attractOn = 0;
+    timeLastEvent = millis();
+    color = "green";
+  }
+
+  //end function checkPinStates
 }
 
 
 
 void attractMode() {
-  pd.adrLED1()->sparkle(color,20,20);
-  pd.adrLED2()->sparkle("blue",80,0);
+  pd.adrLED1()->sparkle(color, 20, 20);
+  pd.adrLED2()->sparkle("blue", 80, 0);
 }
