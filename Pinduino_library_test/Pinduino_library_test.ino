dc447ddcfd4ee047fc08ef03494fa866d8661e6f
Eric Lyons <lyonsden@gmail.com>
2022-10-30 07:23:54 -0700

    updated testing effect

--- Pinduino_library_test/Pinduino_library_test.ino
+++ Pinduino_library_test/Pinduino_library_test.ino
@@ -1,72 +1,25 @@
+//Eric Lyons 2020
+//Note to self:  Play more pinball!
+//Interfaced for pinduino shield v0.3
+//Uses pinduino library
+
 #include <pinduino.h>
 
+int N_LEDS = 196;
+int aLEDNum1 = N_LEDS;
+int aLEDNum2 = N_LEDS;
 
-int aLEDNum1 = 300;
-int aLEDNum2 = 240;
-int aLEDNum3 = 240;
+pinduino pd (aLEDNum1, "Nano");
 
-pinduino pd(aLEDNum1, "nano");
-void setup()
-{
+
+void setup() {
   Serial.begin(115200);
   pd.adrLED1()->clear();
-  pd.adrLED2()->clear();
-  pd.adrLED3()->clear();
-  pd.pinState()->reset();
-//  Serial.println("1");
-//  delay(1000);
 }
 
-
-void loop()
-{
-  pd.pinState()->update();
-  pd.pinState()->print();
-  
-//  pd.rgbLED1()->set("red");
-//  pd.rgbLED2()->set("red");
-//  pd.rgbLED3()->set("red");
-//  pd.rgbLED4()->set("red");
-//  delay(2000);
-//  pd.rgbLED1()->set("green");
-//  pd.rgbLED2()->set("green");
-//  pd.rgbLED3()->set("green");
-//  pd.rgbLED4()->set("green");
-//  delay(2000);
-//  pd.rgbLED1()->set("blue");
-//  pd.rgbLED2()->set("blue");
-//  pd.rgbLED3()->set("blue");
-//  pd.rgbLED4()->set("blue");
-//  delay(2000);
-//  pd.rgbLED1()->set("white");
-//  pd.rgbLED2()->set("white");
-//  pd.rgbLED3()->set("white");
-//  pd.rgbLED4()->set("white");
-//  delay(2000);
-//  pd.adrLED1()->color("red",255);
-//  pd.adrLED2()->color("red",255);
-//  pd.adrLED3()->color("red",255);
-//  delay(2000);
-//  pd.adrLED1()->color("green",255);
-//  pd.adrLED2()->color("green",255);
-//  pd.adrLED3()->color("green",255);
-//  delay(2000);
-//  pd.adrLED1()->color("blue",255);
-//  pd.adrLED2()->color("blue",255);
-//  pd.adrLED3()->color("blue",255);
-//  delay(2000);
-////    
-//  pd.adrLED1()->color("white",255);
-//  delay(2000);
-//  pd.adrLED1()->color("white",250);
-//  delay(2000);
-//  pd.adrLED1()->color("white",200);
-//  delay(2000);
-//  pd.adrLED1()->color("white",150);
-//  delay(2000);
-//  pd.adrLED1()->color("white",100);
-//  delay(2000);
-//  pd.adrLED1()->color("white",50);
-//  delay(2000);
-
+void loop(){
+//  pd.adrLED1()->sparkle("red", 20);
+//  pd.adrLED2()->sparkle("green", 20);
+  pd.adrLED1()->meteorRain(0, 0, 255, 20, 100, true, 0);
+//  Serial.println(1);
 }
